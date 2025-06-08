c0acd51ebe572fd3d79f034f0eeea90ee018d703
Rudolf-AT <rudolf-qa-koerner@a1.net>
2025-06-08 19:08:23 +0200

    test: for testing the report "Overview" when filtering with Cucumber tags

---
+++ src/test/java/starter/ShoppingList.java
@@ -0,0 +1,21 @@
+package starter;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ShoppingList {
+    @Given("Rama is looking at his shopping list")
+    public void ramaIsLookingAtHisTODOList() {
+        
+    }
+
+    @When("he adds {string} to the list")
+    public void heAddsToTheList(String arg0) {
+        
+    }
+
+    @Then("he sees {string} as an item in the shopping list")
+    public void heSeesAsAnItemInTheTODOList(String arg0) {
+    }
+}
